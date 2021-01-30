ba7d34db298150e920d882080ca12accffa8b52b
Franz Maglaque <maglanquefranz@gmail.com>
2021-01-30 18:30:59 +0800

    search when button is clicked

--- app.js
+++ app.js
@@ -1,243 +1,261 @@
 new Vue({
-    el: '#app',
-    data: {
-        products: [
-           {
-              "id": 1,
-              "name":"Intelligent Granite Table",
-              "category":"Tools",
-              "price":"787.00"
-           },
-           {
-              "id": 2,
-              "name":"Handcrafted Rubber Hat",
-              "category":"Games",
-              "price":"232.00"
-           },
-           {
-              "id": 3,
-              "name":"Rustic Concrete Salad",
-              "category":"Jewelery",
-              "price":"115.00"
-           },
-           {
-              "id": 4,
-              "name":"Gorgeous Concrete Pizza",
-              "category":"Garden",
-              "price":"250.00"
-           },
-           {
-              "id": 5,
-              "name":"Refined Plastic Shoes",
-              "category":"Health",
-              "price":"844.00"
-           },
-           {
-              "id": 6,
-              "name":"Awesome Metal Soap",
-              "category":"Tools",
-              "price":"326.00"
-           },
-           {
-              "id": 7,
-              "name":"Intelligent Fresh Mouse",
-              "category":"Home",
-              "price":"783.00"
-           },
-           {
-              "id": 8,
-              "name":"Licensed Soft Keyboard",
-              "category":"Music",
-              "price":"361.00"
-           },
-           {
-              "id": 9,
-              "name":"Fantastic Rubber Pants",
-              "category":"Garden",
-              "price":"786.00"
-           },
-           {
-              "id": 10,
-              "name":"Awesome Rubber Ball",
-              "category":"Automotive",
-              "price":"696.00"
-           },
-           {
-              "id": 11,
-              "name":"Handcrafted Soft Pizza",
-              "category":"Health",
-              "price":"31.00"
-           },
-           {
-              "id": 12,
-              "name":"Practical Soft Chips",
-              "category":"Computers",
-              "price":"795.00"
-           },
-           {
-              "id": 13,
-              "name":"Practical Frozen Shirt",
-              "category":"Kids",
-              "price":"879.00"
-           },
-           {
-              "id": 14,
-              "name":"Unbranded Plastic Car",
-              "category":"Toys",
-              "price":"454.00"
-           },
-           {
-              "id": 15,
-              "name":"Handcrafted Plastic Table",
-              "category":"Shoes",
-              "price":"189.00"
-           },
-           {
-              "id": 16,
-              "name":"Intelligent Plastic Car",
-              "category":"Grocery",
-              "price":"202.00"
-           },
-           {
-              "id": 17,
-              "name":"Ergonomic Wooden Pizza",
-              "category":"Electronics",
-              "price":"801.00"
-           },
-           {
-              "id": 18,
-              "name":"Refined Rubber Pants",
-              "category":"Home",
-              "price":"580.00"
-           },
-           {
-              "id": 19,
-              "name":"Small Frozen Hat",
-              "category":"Music",
-              "price":"654.00"
-           },
-           {
-              "id": 20,
-              "name":"Unbranded Cotton Chips",
-              "category":"Tools",
-              "price":"305.00"
-           },
-           {
-              "id": 21,
-              "name":"Unbranded Plastic Chicken",
-              "category":"Baby",
-              "price":"943.00"
-           },
-           {
-              "id": 22,
-              "name":"Rustic Fresh Pizza",
-              "category":"Toys",
-              "price":"647.00"
-           },
-           {
-              "id": 23,
-              "name":"Ergonomic Metal Tuna",
-              "category":"Industrial",
-              "price":"51.00"
-           },
-           {
-              "id": 24,
-              "name":"Unbranded Frozen Chicken",
-              "category":"Movies",
-              "price":"248.00"
-           },
-           {
-              "id": 25,
-              "name":"Ergonomic Cotton Table",
-              "category":"Baby",
-              "price":"4.00"
-           },
-           {
-              "id": 26,
-              "name":"Handmade Frozen Pants",
-              "category":"Home",
-              "price":"731.00"
-           },
-           {
-              "id": 27,
-              "name":"Rustic Cotton Bike",
-              "category":"Jewelery",
-              "price":"161.00"
-           },
-           {
-              "id": 28,
-              "name":"Licensed Metal Bacon",
-              "category":"Books",
-              "price":"331.00"
-           },
-           {
-              "id": 29,
-              "name":"Practical Cotton Soap",
-              "category":"Books",
-              "price":"861.00"
-           },
-           {
-              "id": 30,
-              "name":"Sleek Frozen Tuna",
-              "category":"Electronics",
-              "price":"375.00"
+   el: '#app',
+   data: {
+       products: [
+          {
+             "id": 1,
+             "name":"Intelligent Granite Table",
+             "category":"Tools",
+             "price":"787.00"
+          },
+          {
+             "id": 2,
+             "name":"Handcrafted Rubber Hat",
+             "category":"Games",
+             "price":"232.00"
+          },
+          {
+             "id": 3,
+             "name":"Rustic Concrete Salad",
+             "category":"Jewelery",
+             "price":"115.00"
+          },
+          {
+             "id": 4,
+             "name":"Gorgeous Concrete Pizza",
+             "category":"Garden",
+             "price":"250.00"
+          },
+          {
+             "id": 5,
+             "name":"Refined Plastic Shoes",
+             "category":"Health",
+             "price":"844.00"
+          },
+          {
+             "id": 6,
+             "name":"Awesome Metal Soap",
+             "category":"Tools",
+             "price":"326.00"
+          },
+          {
+             "id": 7,
+             "name":"Intelligent Fresh Mouse",
+             "category":"Home",
+             "price":"783.00"
+          },
+          {
+             "id": 8,
+             "name":"Licensed Soft Keyboard",
+             "category":"Music",
+             "price":"361.00"
+          },
+          {
+             "id": 9,
+             "name":"Fantastic Rubber Pants",
+             "category":"Garden",
+             "price":"786.00"
+          },
+          {
+             "id": 10,
+             "name":"Awesome Rubber Ball",
+             "category":"Automotive",
+             "price":"696.00"
+          },
+          {
+             "id": 11,
+             "name":"Handcrafted Soft Pizza",
+             "category":"Health",
+             "price":"31.00"
+          },
+          {
+             "id": 12,
+             "name":"Practical Soft Chips",
+             "category":"Computers",
+             "price":"795.00"
+          },
+          {
+             "id": 13,
+             "name":"Practical Frozen Shirt",
+             "category":"Kids",
+             "price":"879.00"
+          },
+          {
+             "id": 14,
+             "name":"Unbranded Plastic Car",
+             "category":"Toys",
+             "price":"454.00"
+          },
+          {
+             "id": 15,
+             "name":"Handcrafted Plastic Table",
+             "category":"Shoes",
+             "price":"189.00"
+          },
+          {
+             "id": 16,
+             "name":"Intelligent Plastic Car",
+             "category":"Grocery",
+             "price":"202.00"
+          },
+          {
+             "id": 17,
+             "name":"Ergonomic Wooden Pizza",
+             "category":"Electronics",
+             "price":"801.00"
+          },
+          {
+             "id": 18,
+             "name":"Refined Rubber Pants",
+             "category":"Home",
+             "price":"580.00"
+          },
+          {
+             "id": 19,
+             "name":"Small Frozen Hat",
+             "category":"Music",
+             "price":"654.00"
+          },
+          {
+             "id": 20,
+             "name":"Unbranded Cotton Chips",
+             "category":"Tools",
+             "price":"305.00"
+          },
+          {
+             "id": 21,
+             "name":"Unbranded Plastic Chicken",
+             "category":"Baby",
+             "price":"943.00"
+          },
+          {
+             "id": 22,
+             "name":"Rustic Fresh Pizza",
+             "category":"Toys",
+             "price":"647.00"
+          },
+          {
+             "id": 23,
+             "name":"Ergonomic Metal Tuna",
+             "category":"Industrial",
+             "price":"51.00"
+          },
+          {
+             "id": 24,
+             "name":"Unbranded Frozen Chicken",
+             "category":"Movies",
+             "price":"248.00"
+          },
+          {
+             "id": 25,
+             "name":"Ergonomic Cotton Table",
+             "category":"Baby",
+             "price":"4.00"
+          },
+          {
+             "id": 26,
+             "name":"Handmade Frozen Pants",
+             "category":"Home",
+             "price":"731.00"
+          },
+          {
+             "id": 27,
+             "name":"Rustic Cotton Bike",
+             "category":"Jewelery",
+             "price":"161.00"
+          },
+          {
+             "id": 28,
+             "name":"Licensed Metal Bacon",
+             "category":"Books",
+             "price":"331.00"
+          },
+          {
+             "id": 29,
+             "name":"Practical Cotton Soap",
+             "category":"Books",
+             "price":"861.00"
+          },
+          {
+             "id": 30,
+             "name":"Sleek Frozen Tuna",
+             "category":"Electronics",
+             "price":"375.00"
+          }
+       ],
+       order:{
+          dir:1,
+          coloumn: 'price'
+       },
+       filters:{
+          name:'',
+          category:'',
+          keywords:''
+       },
+       isSearching:false
+  },
+  computed:{
+     productsSorted(){
+        return this.productsFiltered.sort((a,b) => {
+           let left = a[this.order.coloumn], right = b[this.order.coloumn];
+           if(this.order.coloumn === 'name' || this.order.coloumn === 'category'){
+              if(left < right) return -1 * this.order.dir;  
+              else if(left > right) return 1 * this.order.dir;
+              else return 0;
+           }else{
+              return (left - right) * this.order.dir
            }
-        ],
-        order:{
-           dir:1,
-           coloumn: 'price'
-        },
-        filters:{
-           name:'',
-        }
-   },
-   computed:{
-      productsSorted(){
-         return this.productsFiltered.sort((a,b) => {
-            let left = a[this.order.coloumn], right = b[this.order.coloumn];
-            if(this.order.coloumn === 'name' || this.order.coloumn === 'category'){
-               if(left < right) return -1 * this.order.dir;  
-               else if(left > right) return 1 * this.order.dir;
-               else return 0;
-            }else{
-               return (left - right) * this.order.dir
-            }
-         });
-         // console.log(this.productsFiltered);
-      },
-      sortType(){
-         return this.order.dir === 1 ? 'ascending' : 'descending'
-      },
-      whenSearching(){
-         return this.filters.name.length > 0;
-      },
-      productsFiltered(){
-         let products = this.products;
+        });
+        // console.log(this.productsFiltered);
+     },
+     sortType(){
+        return this.order.dir === 1 ? 'ascending' : 'descending'
+     },
+     whenSearching(){
+        return this.filters.name.length > 0;
+     },
+     productsFiltered(){
+        let products = this.products;
 
-         if(this.filters.name){
-            let findName = new RegExp(this.filters.name,'i')
-            products = products.filter(el => el.name.match(findName))
-            // console.log()
-         }
-         console.log(products);
-         return products;
-      }
-   },
-   methods:{
-      toggle(coloumn){
-         this.order.coloumn = coloumn;
-         console.log(this.order.coloumn);
-         this.order.dir *= -1;
-      },
-      classes(coloumn){
-         return [
-         'sort-control',
-         // this.order.dir === 1 ? 'ascending' : 'descending'
-         this.order.coloumn === coloumn ? this.sortType : ''
-         ]
-      },
-      clearText(){
-         return this.filters.name = '';
-      }
-   }
+        if(this.filters.name){
+           let findName = new RegExp(this.filters.name,'i')
+           products = products.filter(el => el.name.match(findName))
+           // console.log()
+        }
+        console.log(products);
+        return products;
+     },
+     keywordsIsInvalid(){
+        return this.filters.keywords.length < 3;
+     }
+  },
+  methods:{
+     toggle(coloumn){
+        this.order.coloumn = coloumn;
+        console.log(this.order.coloumn);
+        this.order.dir *= -1;
+     },
+     classes(coloumn){
+        return [
+        'sort-control',
+        // this.order.dir === 1 ? 'ascending' : 'descending'
+        this.order.coloumn === coloumn ? this.sortType : ''
+        ]
+     },
+     clearText(){
+      //   return this.filters.name = '';
+         // return this.filters.name = this.filters.keywords = "";
+        this.filters.name = this.filters.keywords = "";
+        this.isSearching = false;
+        console.log(this.isSearching);
+     },
+     search(){
+        if(!this.keywordsIsInvalid){
+         this.filters.name = this.filters.keywords;
+         this.isSearching = true;
+         console.log(this.isSearching);
+        }
+
+     }
+  }
 })
